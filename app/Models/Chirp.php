3adcf3cd0929b4db7f843c6c6f5e778bd9ada12f
Obi Christopher <ikchristo19@gmail.com>
2024-01-19 01:47:23 +0000

    first commit

---
+++ app/Models/Chirp.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Chirp extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'message',
+    ];
+
+    /**
+     * Declare chirps relationship with user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
